3c32a45b030b4558166a4477c54f8d64a4363756
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-10-08 16:29:55 +0700

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-# Misalkan 'svm_model' adalah model Anda dan 'tfidf_vectorizer' adalah vectorizer Anda
-model_dan_vectorizer = {
-    'model': joblib.load('model_svm.joblib'),
-    'vectorizer': joblib.load('vectorizer.joblib')
-}
-
-joblib.dump(model_dan_vectorizer, 'sentiment_model.joblib')
