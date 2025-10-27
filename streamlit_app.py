0cf4e8ef7b22a200b76ddb0cb1648a398dbd60e6
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2025-10-27 12:36:17 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import re
 import nltk
 import plotly.express as px
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
